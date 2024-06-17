7f06d894c37b9ef12ff1cfef8d3ee523e1b9d2f8
kevin-pietruszka <kevinpiet13@gmail.com>
2024-06-17 13:57:27 -0400

    adjust layout structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Header } from "@/components/header";
 import { ThemeProvider } from "next-themes";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -19,10 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className}`}>
-        <ThemeProvider attribute="class">
-          <Header />
-          {children}
-        </ThemeProvider>
+        <ThemeProvider attribute="class">{children}</ThemeProvider>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
-import Image from "next/image";
+import { Header } from "@/components/header";
 
 export default function Home() {
   return (
     <main>
+      <Header />
     </main>
   );
 }
