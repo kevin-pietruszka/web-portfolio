6de49b094e9b4e5590f416e4dbadf5e0e19efe27
kevin-pietruszka <kevinpiet13@gmail.com>
2024-06-17 14:10:48 -0400

    allow mount for dark mode button

--- components/header/theme-switch.tsx
+++ components/header/theme-switch.tsx
@@ -1,10 +1,16 @@
 import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 
 export function ThemeSwitch() {
+  const [isMounted, setMounted] = useState(false);
   const { systemTheme, theme, setTheme } = useTheme();
   const currentTheme = theme === "system" ? systemTheme : theme;
 
-  return (
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  return isMounted ? (
     <button
       onClick={() => setTheme(currentTheme === "dark" ? "light" : "dark")}
       className="bg-gray-200 dark:bg-gray-800 text-gray-800 dark:text-white px-4 py-2 rounded"
@@ -12,5 +18,5 @@ export function ThemeSwitch() {
     >
       {currentTheme === "dark" ? "Light" : "Dark"}
     </button>
-  );
+  ) : null;
 }
