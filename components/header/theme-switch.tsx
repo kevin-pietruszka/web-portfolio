404e43905a245ce2aaf7f1ca45c6028dc7c02821
kevin-pietruszka <kevinpiet13@gmail.com>
2024-06-17 20:55:15 -0400

    slider

--- components/header/theme-switch.tsx
+++ components/header/theme-switch.tsx
@@ -5,18 +5,40 @@ export function ThemeSwitch() {
   const [isMounted, setMounted] = useState(false);
   const { systemTheme, theme, setTheme } = useTheme();
   const currentTheme = theme === "system" ? systemTheme : theme;
+  const toggleTheme = () => {
+    setTheme(currentTheme === "dark" ? "light" : "dark");
+  };
+  const isDarkMode = () => {
+    return theme === "dark";
+  };
 
   useEffect(() => {
     setMounted(true);
   }, []);
 
   return isMounted ? (
-    <button
-      onClick={() => setTheme(currentTheme === "dark" ? "light" : "dark")}
-      className="bg-gray-200 dark:bg-gray-800 text-gray-800 dark:text-white px-4 py-2 rounded"
-      suppressHydrationWarning
-    >
-      {currentTheme === "dark" ? "Light" : "Dark"}
-    </button>
-  ) : null;
+    <label className="relative inline-flex items-center cursor-pointer">
+      <input
+        type="checkbox"
+        className="sr-only peer"
+        checked={isDarkMode()}
+        onChange={toggleTheme}
+      />
+      <div className="w-14 h-8 rounded-full bg-gray-400 relative transition-all duration-700">
+        <div
+          className={`absolute top-1 left-1 w-6 h-6 rounded-full bg-white dark:bg-gray-600 transition-all duration-700 ${
+            isDarkMode() ? "translate-x-6" : "translate-x-0"
+          }`}
+        >
+          {isDarkMode() ? (
+            <span className="flex items-center justify-center h-full">🌑</span>
+          ) : (
+            <span className="flex items-center justify-center h-full">☀️</span>
+          )}
+        </div>
+      </div>
+    </label>
+  ) : (
+    <div className="w-14 h-8 rounded-full bg-gray-400 relative transition-all duration-700"></div>
+  );
 }
