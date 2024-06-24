285d3709924a85d8afdb429acdbdc525dd263d36
kevin-pietruszka <kevinpiet13@gmail.com>
2024-06-23 21:21:12 -0400

    Simple logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,9 @@
 import Logo from "./header/Logo";
 
 export default function Header() {
-  return <header className="p-6 sticky top-0 shadow-sm"></header>;
+  return (
+    <header className="p-6 sticky top-0 shadow-sm">
+      <Logo />
+    </header>
+  );
 }
---
+++ components/header/Logo.tsx
@@ -0,0 +1,7 @@
+import Link from "next/link";
+
+export default function Logo() {
+  return <Link href="/">
+    <h1 className="text-2xl"> {"Kevin"} </h1>
+  </Link>;
+}
