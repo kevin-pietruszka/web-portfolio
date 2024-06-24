930cad67ddc016c8952d123fb0738d9c94cdffdd
kevin-pietruszka <kevinpiet13@gmail.com>
2024-06-24 18:20:11 -0400

    Reworked logo and header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,11 @@
-import Logo from "./header/Logo";
+import Logo from "@/components/header/Logo";
 
 export default function Header() {
   return (
-    <header className="p-6 sticky top-0 shadow-sm">
-      <Logo />
+    <header className="py-8 sticky top-0 shadow-md">
+      <div className="container mx-auto">
+        <Logo />
+      </div>
     </header>
   );
 }
--- components/header/Logo.tsx
+++ components/header/Logo.tsx
@@ -1,7 +1,12 @@
 import Link from "next/link";
 
 export default function Logo() {
-  return <Link href="/">
-    <h1 className="text-2xl"> {"Kevin"} </h1>
-  </Link>;
+  return (
+    <Link href="/">
+      <h1 className="text-2xl font-bold">
+        <span>{"@"}</span>
+        {"Kevin"}
+      </h1>
+    </Link>
+  );
 }
