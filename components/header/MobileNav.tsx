b26141eee9458c441c6131d0abeed44e264ccbf7
kevin-pietruszka <kevinpiet13@gmail.com>
2024-06-27 14:34:31 -0400

    responsive nav menus for dektop and mobile

--- components/header/MobileNav.tsx
+++ components/header/MobileNav.tsx
@@ -1,3 +1,49 @@
+"use client";
+import { links } from "@/lib/constants";
+import Link from "next/link";
+import { useState } from "react";
+import { FaBars, FaSun, FaX } from "react-icons/fa6";
+
 export default function MobileNav() {
-  return <div className="lg:hidden"> Mobile Nav </div>;
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNav = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="md:hidden">
+      <button onClick={toggleNav} className="flex justify-center items-center">
+        <FaBars size={"1.5rem"} />
+      </button>
+
+      {isOpen && (
+        <div className="fixed inset-0 bg-black bg-opacity-50 z-50">
+          <div className="fixed right-0 top-0 h-full w-1/2 bg-white">
+            <div className="py-9 px-6 flex justify-between shadow-md">
+              <button>
+                <FaSun size={"1.5rem"} />
+              </button>
+              <button onClick={toggleNav}>
+                <FaX size={"1.5rem"} />
+              </button>
+            </div>
+            <nav className="pt-9 flex flex-col gap-4 items-center justify-center">
+              {links.map((link, index, array) => {
+                return (
+                  <Link onClick={toggleNav} key={index} href={link.href}>
+                    {link.name}
+                  </Link>
+                );
+              })}
+            </nav>
+          </div>
+          <div
+            className="fixed left-0 top-0 h-full w-1/2 z-40"
+            onClick={toggleNav}
+          ></div>
+        </div>
+      )}
+    </div>
+  );
 }
