d7ceb3276044764dd6515a9078bae861192747bd
kevin-pietruszka <kevinpiet13@gmail.com>
2024-06-30 18:09:17 -0400

    dark mode to selector

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,7 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   plugins: [],
+  darkMode: "class",
   theme: {
     colors: {
       background: 'hsl(var(--background) / <alpha-value>)',
