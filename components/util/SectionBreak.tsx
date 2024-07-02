10200f30180efa88648df08e14705331af9e19d1
kevin-pietruszka <kevinpiet13@gmail.com>
2024-07-01 22:43:49 -0400

    adds bold to each section header

--- components/util/SectionBreak.tsx
+++ components/util/SectionBreak.tsx
@@ -1,7 +1,7 @@
 export default function SectionBreak({ header }: { header: string }) {
   return (
     <div className="flex items-center justify-between gap-4 mb-8">
-      <h1 className="text-4xl">{header}</h1>
+      <h1 className="text-4xl font-bold">{header}</h1>
       <div className="h-px bg-foreground w-full"></div>
     </div>
   );
