958eaf530598c5f8b88564d12ff7acecdad3885c
kevin-pietruszka <kevinpiet13@gmail.com>
2024-07-01 20:08:55 -0400

    Add project section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Header from "@/components/Header";
 import About from "@/components/About";
 import Experience from "@/components/Experience";
+import Projects from "@/components/Projects";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
      <Header /> 
      <About />
      <Experience />
+     <Projects />
     </main>
   );
 }
