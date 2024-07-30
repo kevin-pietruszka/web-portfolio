bb51a605d199dfd8740d3f6d533d02f786d33aa8
Kevin Pietruszka <kevinpiet13@gmail.com>
2024-07-30 15:50:54 -0400

    move background to local variable

--- components/experience/Background.tsx
+++ components/experience/Background.tsx
@@ -1,5 +1,7 @@
-import { background } from "@/lib/constants";
 
 export default function Background() {
+  const background = `
+My journey as a Software Engineer began in High School when I first took a programming class where I learned python and fell in love. Since then, I have graduated from Georgia Tech where I got a Bachelor's and Master's in Computer Science. My coursework covered a wide range of topics such as: Data Structures, Algorithms, Object-Oriented Design, Artificial Intelligence, Machine Learning, Networking, Computer Vision, Security, Graphics, Databases, Data Visualization & Analytics, Distributed Systems, and High Performance Computing. I have also done two internships where I tried to expand my knowledge in the field and gain professional experience. They can be seen in this section.
+`;
   return <p className="mb-16 lg:mb-0 lg:pr-24"> {background} </p>;
 }
--- lib/constants.ts
+++ lib/constants.ts
@@ -36,10 +36,6 @@ export const experiences = [
   },
 ];
 
-export const background = `
-My journey as a Software Engineer began in High School when I first took a programming class where I learned python and fell in love. Since then, I have graduated from Georgia Tech where I got a Bachelor's and Master's in Computer Science. My coursework covered a wide range of topics such as: Data Structures, Algorithms, Object-Oriented Design, Artificial Intelligence, Machine Learning, Networking, Computer Vision, Security, Graphics, Databases, Data Visualization & Analytics, Distributed Systems, and High Performance Computing. I have also done two internships where I tried to expand my knowledge in the field and gain professional experience. They can be seen in this section.
-`;
-
 export const projects = [
   {
     title: "Advising Pathways",
