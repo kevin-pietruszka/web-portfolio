97108579993baf5b1033987d2922e07932fc83dc
Kevin Pietruszka <kevinpiet13@gmail.com>
2024-08-05 10:39:44 -0400

    change project card to use project type

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -8,7 +8,7 @@ export default function Projects() {
       <SectionBreak header="Projects"></SectionBreak>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
           {projects.map((project, index) => (
-            <ProjectCard key={index} {...project} />
+            <ProjectCard key={index} project={project} />
           ))}
         </div>
     </section>
--- components/projects/ProjectCard.tsx
+++ components/projects/ProjectCard.tsx
@@ -1,14 +1,14 @@
+import { Project } from "@/lib/types";
+
 export default function ProjectCard({
-  name,
-  description,
+  project
 }: {
-  name: string;
-  description: string;
+  project: Project;
 }) {
   return (
     <div className="bg-secondary rounded p-4 ">
-      <h3 className="text-xl font-semibold text-primary mb-2">{name}</h3>
-      <p className="text-secondary-foreground">{description}</p>
+      <h3 className="text-xl font-semibold text-primary mb-2">{project.name}</h3>
+      <p className="text-secondary-foreground">{project.description}</p>
     </div>
   );
 }
