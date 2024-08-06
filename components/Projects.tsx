a4c854a6faabe76e0214ee3ee91dcd000c9bc05d
Kevin Pietruszka <kevinpiet13@gmail.com>
2024-08-06 13:12:43 -0400

    change project section to display featured

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,14 +1,14 @@
 import SectionBreak from "./util/SectionBreak";
-import ProjectCard from "./projects/ProjectCard";
-import { projects } from "@/lib/constants";
+import { featured_projects } from "@/lib/constants";
+import FeaturedProjectCard from "./projects/FeaturedProjectCard";
 
 export default function Projects() {
   return (
     <section id="projects" className="container mx-auto h-full p-8">
       <SectionBreak header="Projects"></SectionBreak>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-          {projects.map((project, index) => (
-            <ProjectCard key={index} project={project} />
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-6">
+          {featured_projects.map((project, index) => (
+            <FeaturedProjectCard key={index} project={project} />
           ))}
         </div>
     </section>
