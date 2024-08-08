df27a92af393a4c8279a6177e26ab39f743b86c9
Kevin Pietruszka <kevinpiet13@gmail.com>
2024-08-08 15:27:14 -0400

    projects page

---
+++ app/projects/page.tsx
@@ -0,0 +1,55 @@
+import Link from "next/link";
+import { FaArrowLeft, FaGithub } from "react-icons/fa6";
+import { projects } from "@/lib/constants";
+import Tags from "@/components/util/Tags";
+
+export default function Projects() {
+  return (
+    <section className="container mx-auto p-8">
+      <Link href={"/#projects"} className="relative group hover:text-primary transition-colors duration-200">
+        <span className="inline-flex gap-1 items-center">
+          <FaArrowLeft />
+          {"Back"}
+        </span>
+        <span className="absolute bottom-0 left-0 w-full h-0.5 bg-primary scale-x-0 group-hover:scale-x-100 transition-transform duration-200 origin-left"></span>
+      </Link>
+
+      <h1 className="text-4xl tracking-tight"> {"All Projects"} </h1>
+
+      <table className="border-collapse w-full mt-4 ">
+        <thead className="text-center sticky top-0 z-10 border-b border-accent text-sm text-secondary-foreground backdrop-blur bg-accent/80">
+          <tr>
+            <th className="p-2 font-semibold">
+              Name
+            </th>
+            <th className="p-2 font-semibold">
+              Description
+            </th>
+            <th className="hidden lg:table-cell p-2 font-semibold">
+              Technologies
+            </th>
+            <th className="hidden md:table-cell p-2 font-semibold">
+              Repo
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {projects.map((project, index) => {
+            return (
+              <tr key={index} className="align-top border-b border-accent last:border-none even:bg-accent/40">
+                <td className="p-2"> {project.name} </td>
+                <td className="p-2"> {project.description} </td>
+                <td className="hidden lg:table-cell p-2"> <Tags collection={project.technologies} /> </td>
+                <td className="hidden md:flex p-2 justify-center">
+                  {project.is_public ?
+                    <Link href={project.github_link} target="_blank"> <FaGithub /> </Link> : ""
+                  }
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </section>
+  );
+}
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,16 +1,25 @@
 import SectionBreak from "./util/SectionBreak";
 import { featured_projects } from "@/lib/constants";
 import FeaturedProjectCard from "./projects/FeaturedProjectCard";
+import Link from "next/link";
+import { FaArrowRight } from "react-icons/fa6";
 
 export default function Projects() {
   return (
     <section id="projects" className="container mx-auto h-full p-8">
       <SectionBreak header="Projects"></SectionBreak>
-      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-6">
+      <div className="pb-6 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-6">
         {featured_projects.map((project, index) => (
           <FeaturedProjectCard key={index} project={project} />
         ))}
       </div>
+      <Link href={"/projects"} className="relative group hover:text-primary transition-colors duration-200">
+        <span className="inline-flex items-center gap-2"> 
+          <p> {"View All Projects"} </p> 
+          <FaArrowRight /> 
+        </span>
+        <span className="absolute bottom-0 left-0 w-full h-0.5 bg-primary scale-x-0 group-hover:scale-x-100 transition-transform origin-left"></span>
+      </Link>
     </section>
   );
 }
