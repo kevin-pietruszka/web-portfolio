cb3251f425fad09738414a5e21fb73dad243c6b3
Kevin Pietruszka <kevinpiet13@gmail.com>
2024-08-19 09:52:37 -0400

    fix typo and format file

--- components/util/Tags.tsx
+++ components/util/Tags.tsx
@@ -1,8 +1,8 @@
 import Tag from "@/components/util/Tag";
 
-export default function Tags( {collection}: {collection: string[]}) {
+export default function Tags({ collection }: { collection: string[] }) {
   return (
-    <div className="flex items-start alignt-top flex-row shrink-0 grow-0 flex-wrap gap-2">
+    <div className="flex items-start align-top flex-row shrink-0 grow-0 flex-wrap gap-2">
       {collection.map((tag, index) => {
         return <Tag key={index} word={tag} />;
       })}
